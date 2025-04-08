87caa63ee16e97c332e35b2e9c92349f8902fea8
Kurtozorus <olivier.marchand26@hotmail.fr>
2025-04-08 17:34:28 +0200

    PictureController premiere méthode

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\PictureRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PictureRepository::class)]
 class Picture
@@ -11,18 +13,28 @@ class Picture
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['food:read', 'restaurant:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['food:read', 'restaurant:read'])]
     private ?string $publicPath = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['food:read', 'restaurant:read'])]
     private ?string $localPath = null;
 
     #[ORM\Column(length: 32)]
+    #[Groups(['food:read', 'restaurant:read'])]
     private ?string $title = null;
 
+    #[Assert\NotBlank(message: "Slug cannot be empty.")]
+    #[Assert\Regex(
+        pattern: "/^[a-z0-9-]+$/",
+        message: "Slug must contain only lowercase letters, numbers, and hyphens."
+    )]
     #[ORM\Column(length: 64)]
+    #[Groups(['food:read', 'restaurant:read'])]
     private ?string $slug = null;
 
     #[ORM\Column]
@@ -31,11 +43,11 @@ class Picture
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
-    #[ORM\ManyToOne(inversedBy: 'pictures')]
+    #[ORM\ManyToOne(inversedBy: 'picture')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Restaurant $restaurant = null;
 
-    #[ORM\ManyToOne(inversedBy: 'pictures')]
+    #[ORM\ManyToOne(inversedBy: 'picture')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Food $food = null;
 
